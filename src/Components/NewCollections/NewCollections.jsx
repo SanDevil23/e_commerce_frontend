b5cc5812cf9f7e859652e0db4f545b902c036e48
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-05 22:54:22 +0530

    collection fixed

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './NewCollections.css'
 import new_collection from '../Assets/new_collections'
+import Item from '../Item/Item'
 
 const NewCollections = () => {
   return (
@@ -9,7 +10,7 @@ const NewCollections = () => {
       <hr />
       <div className="collections">
         {new_collection.map((item, i)=>{
-            return <item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
         })}
       </div>
     </div>
