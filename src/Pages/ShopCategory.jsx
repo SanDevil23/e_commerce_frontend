6c59c41946e59263b5867a6d87bb794d50abd535
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-12 22:33:31 +0530

    bugs generated by adding banners

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import { useContext, props } from 'react'
+
 
 const ShopCategory = () => {
+  const {all_product} = useContext(ShopContext);
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
     </div>
   )
 }
